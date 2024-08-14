d630bc5c18bd48cbbbbfd4f04184f06037626bf8
dchassin <dchassin@slac.stanford.edu>
2024-08-14 16:17:21 -0700

    Update scenario.py

--- Task6/scenario.py
+++ Task6/scenario.py
@@ -18,58 +18,14 @@ def __(mo):
 
 @app.cell
 def __(
-    city_ui,
-    county_ui,
-    mo,
-    number_chargers_ui,
-    number_evs_ui,
-    public_fraction_ui,
-    public_rate_ui,
-    public_tariff_ui,
-    residential_fraction_ui,
-    residential_rate_ui,
-    residential_tariff_ui,
-    state_ui,
-    substation_ui,
-    workplace_fraction_ui,
-    workplace_rate_ui,
-    workplace_tariff_ui,
-    year_ui,
-):
-    substations = substation_ui.value.ZIP.to_dict()
-    substation_names = ", ".join(substations.keys()) if substations else None
-    overview_items = mo.md(f"""<table>
-    <caption>Current scenario data<hr/><hr/></caption>
-
-    <tr><th><i>Location</i><hr/></th><td>&nbsp;<hr/></td></tr>
-    <tr><td>City, County, State</td><td>{city_ui.value.title()}, {county_ui.value.title()}, {state_ui.value}</td></tr>
-    <tr><td>Substations</td><td>{"<br/>".join([x[0]+" ("+x[1]+")" for x in substations.items()])}</td></tr>
-
-    <tr><th><i>Demand</i><hr/></th><td>&nbsp;<hr/></td></tr>
-    <tr><td>Study year</td><td>{year_ui.value}</td></tr>
-    <tr><td>Number of EVs</td><td>{number_evs_ui.value:,.0f}</td></tr>
-    <tr><td>Number of chargers</td><td>{number_chargers_ui.value:,.0f}</td></tr>
-
-    <tr><th><i>Supply</i><hr/></th><td>&nbsp;<hr/></td></tr>
-    <tr><th>Public</th><td>{public_fraction_ui.value}% {public_tariff_ui.value}<br/>{public_rate_ui.value}</td></tr>
-    <tr><th>Workplace</th><td>{workplace_fraction_ui.value}% {workplace_tariff_ui.value}<br/>{workplace_rate_ui.value}</td></tr>
-    <tr><th>Residential</th><td>{residential_fraction_ui.value}% {residential_tariff_ui.value}<br/>{residential_rate_ui.value}</td></tr>
-
-    <tr><th><i>Feeders</i><hr/></th><td>&nbsp;<hr/></td></tr>
-
-    <tr><th><i>Loads</i><hr/></th><td>&nbsp;<hr/></td></tr>
-    </table>""")
-    return overview_items, substation_names, substations
-
-
-@app.cell
-def __(
     charger_items,
     feeder_items,
     loadshape_items,
     location_items,
     mo,
     overview_items,
+    renewable_items,
+    storage_items,
     tariff_items,
 ):
     #
@@ -82,12 +38,173 @@ def __(
             "**Demand**": charger_items,
             "**Supply**": tariff_items,
             "**Feeders**": feeder_items,
+            "**Renewables**": renewable_items,
+            "**Storage**": storage_items,
             "**Loads**": loadshape_items,
         },
         multiple=True,
         lazy=True,
     )
     return
+
+
+@app.cell
+def __(
+    base,
+    city_ui,
+    commercial_efficiency_ui,
+    commercial_electrification_gas_ui,
+    county_ui,
+    dt,
+    ev_adoption_rates,
+    ev_adoption_ui,
+    export_capacity_ui,
+    import_capacity_ui,
+    load,
+    load_fraction_ui,
+    mo,
+    number_chargers_ui,
+    number_evs_ui,
+    public_fraction_ui,
+    public_rate_ui,
+    public_tariff_ui,
+    residential_efficiency_ui,
+    residential_electrification_gas_ui,
+    residential_electrification_oil_ui,
+    residential_electrification_propane_ui,
+    residential_fraction_ui,
+    residential_load_growth_ui,
+    residential_rate_ui,
+    residential_tariff_ui,
+    state_ui,
+    substation_ui,
+    workplace_fraction_ui,
+    workplace_rate_ui,
+    workplace_tariff_ui,
+    year_ui,
+):
+    substations = substation_ui.value.ZIP.to_dict()
+    substation_names = ", ".join(substations.keys()) if substations else None
+    _import_margin = 100-load["electric[kW]"].max()*load_fraction_ui.value/100/1000/import_capacity_ui.value*100
+    overview_items = mo.md(f"""<table>
+    <caption>Current scenario data<hr/><hr/></caption>
+
+    <tr><th rowspan=2>Location</th>
+        <th>City, County, State</th>
+        <td>{city_ui.value.title()}, {county_ui.value.title()} County, {state_ui.value}</td>
+        <td></td>
+    </tr>
+    <tr><th>Substations</th>
+        <td>{"<br/>".join([x[0]+" ("+x[1]+")" for x in substations.items()]) if substation_names else '<font color="red"><b>NONE</b></font>'}</td>
+        <td>{'<font color="red">&leftarrow; Select one or more substations</font>' if not substation_names else ''}</td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+
+    <tr><th rowspan=4>Demand</th>
+        <th>Study year</th>
+        <td>{year_ui.value}</td>
+        <td></td>
+    </tr>
+    <tr><th>EV adoption rate</th>
+        <td>{ {y:x for x,y in ev_adoption_rates.items()}[ev_adoption_ui.value]}</td>
+        <td></td>
+    </tr>
+    <tr><th>Number of EVs</th>
+        <td>{number_evs_ui.value:,.0f}</td>
+        <td></td>
+    </tr>
+    <tr><th>Number of chargers</th>
+        <td>{number_chargers_ui.value:,.0f}</td>
+        <td></td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+
+    <tr><th rowspan=3>Supply</th>
+        <th>Public</th>
+        <td>{public_fraction_ui.value}% on {public_tariff_ui.value.lower()} tariff<br/>{public_rate_ui.value}</td>
+        <td></td>
+    </tr>
+    <tr><th>Workplace</th>
+        <td>{workplace_fraction_ui.value}% on {workplace_tariff_ui.value.lower()} tariff<br/>{workplace_rate_ui.value}</td>
+        <td></td>
+    </tr>
+    <tr><th>Residential</th>
+        <td>{residential_fraction_ui.value}% on {residential_tariff_ui.value.lower()} tariff<br/>{residential_rate_ui.value}</td>
+        <td></td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+
+    <tr><th rowspan=2>Feeders</th>
+        <th>Feeder import capacity</th>
+        <td>{import_capacity_ui.value} MW
+            ({_import_margin:.0f}% left)
+        </td>
+        <td>{"<font color=red><b>&leftarrow; Insufficient capacity<br/>Increase import limit or<br/>decrease load fraction.</b></font>" if _import_margin<=0 else ""}</td>
+    </tr>
+    <tr><th>Feeder export capacity</th>
+        <td>{export_capacity_ui.value} MW</td>
+        <td></td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+        
+    <tr><th rowspan=2>Renewables</th>
+        <th>TODO</th>
+        <td></td>
+    </tr>
+    <tr><th>TODO</th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+        
+    <tr><th rowspan=2>Storage</th>
+        <th>TODO</th>
+        <td></td>
+    </tr>
+    <tr><th>TODO</th>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+
+    <tr><th rowspan=6>Loads</th>
+        <th>Load growth</th>
+        <td>{pow((1+residential_load_growth_ui.value/100),int(year_ui.value)-dt.datetime.now().year)*100:.0f}%</td>
+        <td></td>
+    </tr>
+
+    <tr><th>Electrification impact</th>
+        <td>Residential natural gas: {residential_electrification_gas_ui.value/residential_efficiency_ui.value*100:.0f}%<br/>
+            Residential fuel oil: {residential_electrification_oil_ui.value/residential_efficiency_ui.value*100:.0f}%<br/>
+            Residential propane: {residential_electrification_propane_ui.value/residential_efficiency_ui.value*100:.0f}%<br/>
+            Commercial natural gas: {commercial_electrification_gas_ui.value/commercial_efficiency_ui.value*100:.0f}%<br/>
+        </td>
+        <td></td>
+    </tr>
+
+    <tr><th>Median load</th>
+        <td>{load["electric[kW]"].median()/1000:.1f} MW ({load["electric[kW]"].median()/base["electric[kW]"].median()*100-100:+.0f}%)</td>
+        <td></td>
+    </tr>
+
+    <tr><th>Average load</th>
+        <td>{load["electric[kW]"].mean()/1000:.1f} MW ({load["electric[kW]"].mean()/base["electric[kW]"].mean()*100-100:+.0f}%)</td>
+        <td></td>
+    </tr>
+
+    <tr><th>Peak load</th>
+        <td>{load["electric[kW]"].max()/1000:.1f} MW ({load["electric[kW]"].max()/base["electric[kW]"].max()*100-100:+.0f}%)</td>
+        <td></td>
+    </tr>
+
+    <tr><th>Light load</th>
+        <td>{load["electric[kW]"].min()/1000:.1f} MW ({load["electric[kW]"].min()/base["electric[kW]"].min()*100-100:+.0f}%)</td>
+        <td></td>
+    </tr>
+    <tr><td colspan=4><hr/></td></tr>
+
+    </table>""")
+    return overview_items, substation_names, substations
 
 
 @app.cell
@@ -105,24 +222,19 @@ def __(pd):
 
 
 @app.cell
-def __(defaults, mo, substation_data):
+def __(defaults, json, mo):
     #
     # State selection
     #
-    _values = (
-        substation_data[substation_data["COUNTYFIPS"] != "NOT AVAILABLE"]
-        .index.get_level_values(0)
-        .unique()
-        .tolist()
-    )
+    utilities_data = json.load(open("utilities.json","r"))
     state_ui = mo.ui.dropdown(
         label="State:",
-        options=_values,
+        options=list(utilities_data),
         value=(defaults['STATE'] if 'STATE' in defaults else _values[0]),
         # on_change=set_state,
         allow_select_none=False,
     )
-    return state_ui,
+    return state_ui, utilities_data
 
 
 @app.cell
@@ -247,17 +359,22 @@ def __(
 
 @app.cell
 def __(mo):
+    #
+    # EV adoption rate
+    #
+    ev_adoption_rates = {
+        "Low (100-1,000)": 1000,
+        "Medium (1,000-10,000)": 10000,
+        "High (10,000-100,000)": 100000,
+        "Very high (100,000-1,000,000)": 1000000,
+        }
     ev_adoption_ui = mo.ui.dropdown(
         label="EV adoption:",
-        value="Low (100-1,000)",
-        options={"Low (100-1,000)":1000,
-                 "Medium (1,000-10,000)":10000,
-                 "High (10,000-100,000)":100000,
-                 "Very high (100,000-1,000,000)":1000000,
-        },
+        value=list(ev_adoption_rates)[0],
+        options=ev_adoption_rates,
         allow_select_none=False,
     )
-    return ev_adoption_ui,
+    return ev_adoption_rates, ev_adoption_ui
 
 
 @app.cell
@@ -340,16 +457,15 @@ def __(pd):
 
 
 @app.cell
-def __(defaults, json, mo, state_ui, tariff_data):
+def __(defaults, mo, state_ui, tariff_data, utilities_data):
     #
     # Read state utility database
     #
-    _utilities = json.load(open("utilities.json","r"))
-    _utilities = [x for x in _utilities[state_ui.value] if x in tariff_data.index.get_level_values(0).unique()]
+    _utilities = [x for x in utilities_data[state_ui.value] if x in tariff_data.index.get_level_values(0).unique()]
     utility_ui = mo.ui.dropdown(
         label="Utility name:",
         options=_utilities,
-        value=defaults['UTILITY'] if 'UTILITY' in defaults else _utilities[0],
+        value=defaults['UTILITY'] if 'UTILITY' in defaults else utilities_data[0],
         allow_select_none=False,
     )
     return utility_ui,
@@ -525,10 +641,47 @@ def __():
 
 
 @app.cell
-def __(mo):
+def __(math, mo, substation_ui):
+    _value=max(10,sum([x*y*(int(x/100)+1) for x,y in substation_ui.value[['MAX_VOLT','LINES']].values]))
+    _max=10**int(math.log10(_value)+1)
+    import_capacity_ui = mo.ui.slider(
+        label="Import limit (MW)",
+        start=0,
+        stop=_max,
+        value=_value,
+        debounce=True,
+        show_value=True,
+    )
+    export_capacity_ui = mo.ui.slider(
+        label="Export limit (MW)",
+        start=0,
+        stop=_max,
+        value=0,
+        debounce=True,
+        show_value=True,
+    )
+    return export_capacity_ui, import_capacity_ui
+
+
+@app.cell
+def __(import_capacity_ui, load, mo):
+    load_fraction_ui = mo.ui.slider(
+        label=f"""Fraction of peak load served (% of {load["electric[kW]"].max()/1000:.1f} MW)""",
+        start=0,
+        stop=100,
+        value=min(100,round(import_capacity_ui.value/(load["electric[kW]"].max()/1000)*100,0)),
+        debounce=True,
+        show_value=True,
+    )
+    return load_fraction_ui,
+
+
+@app.cell
+def __(export_capacity_ui, import_capacity_ui, load_fraction_ui, mo):
+
     feeder_items = mo.vstack([
-        mo.md("""Set constraints on your feeders and select the network assets to include in the hotspot analysis."""),
-        mo.md("TODO")
+        mo.md("""Set constraints on the network assets to include in the hotspot analysis."""),
+        mo.hstack([import_capacity_ui,export_capacity_ui,load_fraction_ui]),
     ])
     return feeder_items,
 
@@ -542,12 +695,174 @@ def __():
 
 
 @app.cell
-def __(mo):
+def __(loads, mo):
+    residential_load_growth_ui = mo.ui.slider(
+        label="Residential load growth (%/y)",
+        start=0.0,
+        stop=20.0,
+        step=0.5,
+        value=loads.ASSUMPTIONS["residential"]["growth"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    residential_electrification_oil_ui = mo.ui.slider(
+        label="Residential fuel oil electrification (%)",
+        start=0.0,
+        stop=100.0,
+        step=1.0,
+        value=loads.ASSUMPTIONS["residential"]["electrification"]["oil"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    residential_electrification_gas_ui = mo.ui.slider(
+        label="Residential natural electrification (%)",
+        start=0.0,
+        stop=100.0,
+        step=1.0,
+        value=loads.ASSUMPTIONS["residential"]["electrification"]["gas"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    residential_electrification_propane_ui = mo.ui.slider(
+        label="Residential propane electrification (%)",
+        start=0.0,
+        stop=100.0,
+        step=1.0,
+        value=loads.ASSUMPTIONS["residential"]["electrification"]["propane"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    residential_efficiency_ui = mo.ui.slider(
+        label="Residential electrification efficiency (%)",
+        start=1.0,
+        stop=100.0,
+        step=1.0,
+        value=loads.ASSUMPTIONS["residential"]["efficiency"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    commercial_load_growth_ui = mo.ui.slider(
+        label="Commercial load growth (%/y)",
+        start=0.0,
+        stop=20.0,
+        step=0.5,
+        value=loads.ASSUMPTIONS["commercial"]["growth"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    commercial_electrification_gas_ui = mo.ui.slider(
+        label="Commercial natural gas electrification (%)",
+        start=0.0,
+        stop=100.0,
+        step=1.0,
+        value=loads.ASSUMPTIONS["commercial"]["electrification"]["gas"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    commercial_efficiency_ui = mo.ui.slider(
+        label="Commercial electrification efficiency (%)",
+        start=1.0,
+        stop=100.0,
+        step=1.0,
+        value=loads.ASSUMPTIONS["commercial"]["efficiency"]*100,
+        debounce=True,
+        show_value=True,
+    )
+    return (
+        commercial_efficiency_ui,
+        commercial_electrification_gas_ui,
+        commercial_load_growth_ui,
+        residential_efficiency_ui,
+        residential_electrification_gas_ui,
+        residential_electrification_oil_ui,
+        residential_electrification_propane_ui,
+        residential_load_growth_ui,
+    )
+
+
+@app.cell
+def __(
+    commercial_efficiency_ui,
+    commercial_electrification_gas_ui,
+    commercial_load_growth_ui,
+    load,
+    mo,
+    residential_efficiency_ui,
+    residential_electrification_gas_ui,
+    residential_electrification_oil_ui,
+    residential_electrification_propane_ui,
+    residential_load_growth_ui,
+):
     loadshape_items = mo.vstack([
-        mo.md("""Fine-tune the future end-use load shapes based on region, year, and technology adoption rates."""),
-        mo.md("TODO")    
-    ])
+        mo.hstack([residential_load_growth_ui,commercial_load_growth_ui]),
+        mo.hstack([residential_electrification_gas_ui,commercial_electrification_gas_ui]),
+        mo.hstack([residential_electrification_oil_ui]),
+        mo.hstack([residential_electrification_propane_ui]),
+        mo.hstack([residential_efficiency_ui,commercial_efficiency_ui]),
+        (load["electric[kW]"]/1000).plot(grid=True,linewidth=0.5,figsize=(15,5),xlabel='Load (MW)')
+    ])    
     return loadshape_items,
+
+
+@app.cell
+def __(
+    commercial_efficiency_ui,
+    commercial_electrification_gas_ui,
+    commercial_load_growth_ui,
+    county_ui,
+    dt,
+    loads,
+    residential_efficiency_ui,
+    residential_electrification_gas_ui,
+    residential_electrification_oil_ui,
+    residential_electrification_propane_ui,
+    residential_load_growth_ui,
+    state_ui,
+    year_ui,
+):
+    base = loads.get_forecast(dt.datetime.now().year,state=state_ui.value,county=county_ui.value.title()+" County",assumptions=loads.BASECASE)
+    load = loads.get_forecast(
+        year=int(year_ui.value),
+        state=state_ui.value,
+        county=county_ui.value.title()+" County",
+        assumptions={
+            "residential" : {
+                "growth" : residential_load_growth_ui.value/100,
+                "electrification" : {
+                    "oil" : residential_electrification_oil_ui.value/100,
+                    "gas" : residential_electrification_gas_ui.value/100,
+                    "propane" : residential_electrification_propane_ui.value/100,
+                },
+                "efficiency" : residential_efficiency_ui.value/100,
+            },
+            "commercial" : {
+                "growth" : commercial_load_growth_ui.value/100,        
+                "electrification" : {
+                    "gas" : commercial_electrification_gas_ui.value/100,
+                },
+                "efficiency" : commercial_efficiency_ui.value/100,
+            },
+        }
+    )
+    return base, load
+
+
+@app.cell
+def __(mo):
+    #
+    # Renewables
+    #
+    renewable_items = mo.md("TODO")
+    return renewable_items,
+
+
+@app.cell
+def __(mo):
+    #
+    # Storage
+    #
+    storage_items = mo.md("TODO")
+    return storage_items,
 
 
 @app.cell
@@ -557,7 +872,9 @@ def __():
     import pandas as pd
     import json
     import config
-    return config, dt, json, mo, pd
+    import loads
+    import math
+    return config, dt, json, loads, math, mo, pd
 
 
 if __name__ == "__main__":
